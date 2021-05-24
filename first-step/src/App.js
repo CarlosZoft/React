46be8120255dee333faff28fc1494e357cfb03af
Carlos <carlosoftwar@gmail.com>
2021-05-24 17:36:24 -0300

    Update : Adicionando CSS e limpando component

--- first-step/src/App.js
+++ first-step/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { loadPosts } from './utils/loadPosts'
+import { Posts } from './components/Posts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    counter: 0,
+    posts: []
+  };
+  async componentDidMount() {
+    const postsAndPhotos = await loadPosts();
+    this.setState({ posts: postsAndPhotos })
+  }
+  render() {
+    const { posts } = this.state
+
+    return (
+      <section className="container" >
+        <Posts posts={posts} />
+      </section>
+    )
+  }
+
 }
 
 export default App;
